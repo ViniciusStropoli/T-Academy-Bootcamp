389496b1f3c41db865de86824884aea77e964f54
Vinícius Stropoli <vini.stropoli@gmail.com>
2025-08-19 07:59:09 -0300

    Update BancoDados.java

--- Aula6/src/Trabalho/BancoDados.java
+++ Aula6/src/Trabalho/BancoDados.java
@@ -3,26 +3,26 @@ package Trabalho;
 public class BancoDados {
 
     public static void carregarReceitas() {
-        Receita processador = new Receita(" Processador ");
+        Receita processador = new Receita("Processador");
         processador.adicionarIngrediente("Chip", 1);
         processador.adicionarIngrediente("Metal", 2);
 
-        Receita computador = new Receita(" Computador ");
+        Receita computador = new Receita("Computador");
         computador.adicionarIngrediente("Processador", 1);
         computador.adicionarIngrediente("Placa de Circuito", 2);
         computador.adicionarIngrediente("Gabinete", 1);
 
-        Receita robo = new Receita(" Robô ");
+        Receita robo = new Receita("Robô");
         robo.adicionarIngrediente("Computador", 1);
         robo.adicionarIngrediente("Sensor", 2);
         robo.adicionarIngrediente("Rodas", 4);
 
-        Receita estatuaLiberdade = new Receita(" Estátua da Liberdade ");
+        Receita estatuaLiberdade = new Receita("Estátua da Liberdade");
         estatuaLiberdade.adicionarIngrediente("Cobre", 7);
         estatuaLiberdade.adicionarIngrediente("Aço", 3);
         estatuaLiberdade.adicionarIngrediente("Cimento", 2);
 
-        Receita carro = new Receita(" Carro ");
+        Receita carro = new Receita("Carro");
         carro.adicionarIngrediente("Computador de bordo", 1);
         carro.adicionarIngrediente("Chassi", 1);
         carro.adicionarIngrediente("Rodas", 5);
@@ -50,3 +50,4 @@ public class BancoDados {
 
     }
 }
+
